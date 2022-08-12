7262dc36a7157167f551146e7b6c8381031fa314
kateburash <ylmartiy@gmail.com>
2022-08-12 17:48:57 +0300

    added function solveEquation

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,8 +1,26 @@
+"use strict";
 function solveEquation(a, b, c) {
   let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
-}
+ 
+    if (a===0)
+    return "a=0 недопустимое знанчение";
+  
+    let D=b**2-4*a*c; 
+
+    let x1, x2;
+
+    if (D<0)
+    arr = [];
+    else if (D>0) {
+        x1=(-b + Math.sqrt(D) )/(2*a);
+        x2=(-b - Math.sqrt(D) )/(2*a);
+        arr = [x1, x2];
+    } else if (D===0) {
+        x1=-b/(2*a);
+        arr = [x1];
+    }   // код для задачи №1 писать здесь
+    return arr; // array
+  }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;

2031cd5950e850f6344cad01c7ef1302abed62bf
kateburash <ylmartiy@gmail.com>
2022-08-21 12:09:51 +0300

    added solution of tasks

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,20 @@
 function compareArrays(arr1, arr2) {
-  let result;
 
-  // Ваш код
+  ;
+
+  let result = (arr1.length === arr2.length) && arr1.every(function(number, index) {
+    return number === arr2[index];
+  });
+
+  
 
   return result; // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
+  let resultArr = arr.filter((number) => number > 0)
+  .filter((number) => number % 3 === 0)
+  .map((number) => number * 10);
 
   return resultArr; // array
 }

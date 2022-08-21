3d6ca85e01622e3de2f26c1fec454fe03ca6b878
kateburash <ylmartiy@gmail.com>
2022-08-21 12:12:12 +0300

    Revert "added solution of tasks"
    This reverts commit 2031cd5950e850f6344cad01c7ef1302abed62bf.

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,20 +1,15 @@
 function compareArrays(arr1, arr2) {
+  let result;
 
-  ;
-
-  let result = (arr1.length === arr2.length) && arr1.every(function(number, index) {
-    return number === arr2[index];
-  });
-
-  
+  // Ваш код
 
   return result; // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr = arr.filter((number) => number > 0)
-  .filter((number) => number % 3 === 0)
-  .map((number) => number * 10);
+  let resultArr;
+
+  // Ваш код
 
   return resultArr; // array
 }
